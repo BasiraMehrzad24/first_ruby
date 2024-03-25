8981add73592d9ee4c1da8addf96adaaad8036f2
BasiraMehrzad24 <basiramehrzad25@gmail.com>
2024-03-25 10:36:39 +0430

    sign in form are done

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,21 @@
 class RegistrationsController < ApplicationController
-    def new
-@user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle successful user creation, e.g., redirect to a success page
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      # Handle validation errors, e.g., render the new form with error messages
+      render :new
     end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get "about-us", to:"about#index", as: :about
   get "stock", to:"stock#index"
  get "sign_up", to: "registrations#new"
- post "sign_up",  to:"registrations#create"
+ post "sign_up",  to: "registrations#create"
 root to: "main#index"
 end
